b2656d3a2fabc094affa3c6d099c601490c1797d
pratishabhople <pratikshabhople26@gmail.com>
2022-10-14 22:23:22 +0530

    Check Attendence

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         public static void Main(String[] args)
         {
             Console.WriteLine("Welcome to the Employee Wage Problem");
+            EmployeeCheck.CheckAttendence();
         }
     }
 }
