97712580de550c2088b3e8d4a4775a889fb0a3e9
pratishabhople <pratikshabhople26@gmail.com>
2022-10-14 21:31:06 +0530

    Check Attendence

---
+++ EmployeeCheck.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    public static class EmployeeCheck
+    {
+        public static void CheckAttendence()
+        {
+            int ISFULLTIME = 1;
+
+
+            Random random = new Random();
+            int empCheck = random.Next(2);
+            if (empCheck==ISFULLTIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+
+            }
+        }
+    }
+}
