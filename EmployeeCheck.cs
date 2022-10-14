0c8c13485065df8d41b5d6c71823ceaea98c6532
pratishabhople <pratikshabhople26@gmail.com>
2022-10-14 23:44:12 +0530

    calculate Employee wage

--- EmployeeCheck.cs
+++ EmployeeCheck.cs
@@ -11,18 +11,23 @@ namespace EmployeeWageProblem
         public static void CheckAttendence()
         {
             int ISFULLTIME = 1;
-
+            int EMPRATEPERHOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
 
             Random random = new Random();
             int empCheck = random.Next(2);
             if (empCheck==ISFULLTIME)
             {
                 Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
-
+                empHrs = 0;
+                empWage = empHrs * EMPRATEPERHOUR;
+                Console.WriteLine("Emp Wage:" + empWage);
             }
         }
     }
