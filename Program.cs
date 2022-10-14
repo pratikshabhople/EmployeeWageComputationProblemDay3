b08225b3ec236fd2082d990429f68f21fe3a02c4
pratishabhople <pratikshabhople26@gmail.com>
2022-10-14 19:54:37 +0530

    greeting message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeWageProblem
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to the Employee Wage Problem");
+        }
+    }
+}
